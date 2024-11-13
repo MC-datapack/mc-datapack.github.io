da9eebcf79c43f27e9a474060e464aeeb75bf51d
MC-datapack (Timo_11) <timo.tewes@web.de>
2024-11-13 17:03:47 +0100

    Test 2

--- src/script.js
+++ src/script.js
@@ -1,1 +1,12 @@
-const title = "This website is all about Minecraft Datapacks/Resourcepacks."
+document.addEventListener('DOMContentLoaded', () => {
+    const heading = document.querySelector('h1');
+    heading.textContent = 'This website is all about Minecraft Datapacks/Resourcepacks.'
+
+    const button = document.createElement('button');
+    button.textContent = "Get Started";
+    button.addEventListener('click', () => {
+        alert('You clicked the button!')
+    });
+
+    document.body.appendChild(button);
+})
